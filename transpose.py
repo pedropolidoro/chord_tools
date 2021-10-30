db34ac4ccb398afef35d32f52d7b6075bdaaa994
pedropolidoro <87273469+pedropolidoro@users.noreply.github.com>
2021-10-30 14:17:16 -0300

    Create transpose.py

---
+++ transpose.py
@@ -0,0 +1,25 @@
+import re
+from musicfunctions import note2num, num2note
+
+with open('chord_re.txt', 'r', encoding='UTF-8') as file_obj:
+    chord_re = file_obj.read()
+
+chord_re = re.compile(chord_re, re.X)
+
+def cv_chord(match_obj, dislocate):
+    fundamental, other, bass = match_obj.groups()
+    # remove None values
+    if not bass:
+        bass = ''
+    if not other:
+        other = ''
+    fundamental = (note2num(fundamental) + dislocate) % 12
+    fundamental = num2note(fundamental)
+    if bass:
+        bass = (note2num(bass) + dislocate) % 12
+        bass = num2note(bass)
+        bass = '/' + bass # because regex remove the "/"
+    return fundamental + other + bass
+
+def chord_sub(string, dislocate):
+    return chord_re.sub(lambda x: cv_chord(x, dislocate), string)
