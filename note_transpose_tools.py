af995f9fb0e48aaa03a0d21a3472c2b81886ae9b
pedropolidoro <87273469+pedropolidoro@users.noreply.github.com>
2021-10-30 16:43:08 -0300

    Update note_transpose_tools.py

--- note_transpose_tools.py
+++ note_transpose_tools.py
@@ -1,5 +1,5 @@
 notenames = 'C,,D,,E,F,,G,,A,,B'.split(',')
-accidentals = {'b' : -1, '#'  : 1, 'x': 2, 'X' : 2}
+accidentals = {'b' : -1, '#'  : 1, 'x': 2, 'X' : 2, '♯': 1, '♭' : -1}
 
 def note2num(note):
     number = notenames.index(note[0])
