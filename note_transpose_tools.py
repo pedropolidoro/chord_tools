ba14acfb838aa4a055800a01eb8e173ffc7ec4dc
pedropolidoro <87273469+pedropolidoro@users.noreply.github.com>
2021-10-31 03:07:53 -0300

    Update note_transpose_tools.py

--- note_transpose_tools.py
+++ note_transpose_tools.py
@@ -4,12 +4,10 @@ accidentals = {'b' : -1, '#'  : 1, 'x': 2, 'X' : 2, '♯': 1, '♭' : -1}
 def note2num(note):
 
     number = notenames.index(note[0])
-    if not note[1:]:
-        # When chord name is only one letter.
-        return number
-    # When chord name is more than just one letter.
-    for x in note[1:]:
-        number += accidentals[x]
+    if note[1:]:
+        # When chord name is more than just one letter.
+        for accidental_simbol in note[1:]:
+            number += accidentals[accidental_simbol]
     return number
 
 
