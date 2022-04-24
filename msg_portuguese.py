da2dd1da08e6740414ffcbdb269d2f78de9dd9bc
pedropolidoro <87273469+pedropolidoro@users.noreply.github.com>
2022-04-24 12:18:15 -0300

    Create msg_portuguese.py

---
+++ msg_portuguese.py
@@ -0,0 +1,8 @@
+ctrl_c = 'Copie o texto original (Ctrl + c). Aguardando.' 
+num_semitones = 'Informe o número de semitons a deslocar: '
+fail_number = 'Somente é permitido sinal de negativo e algarismos.'
+what_accidental = 'Informe o acidente a utilizar: '
+accidental_fail = 'Como acidente somente é permitido # ou b'
+ctrl_v = ''''Transposição realizada.\n
+A versão transposta foi armazenada na área de transferência.\n
+(Utilize opção colar ou Ctrl + v)'''
