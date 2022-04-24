773fd852f550e5b686e12efe9135b5fd32a236dd
pedropolidoro <87273469+pedropolidoro@users.noreply.github.com>
2022-04-24 12:32:25 -0300

    Update msg_portuguese.py

--- msg_portuguese.py
+++ msg_portuguese.py
@@ -3,6 +3,6 @@ num_semitones = 'Informe o número de semitons a deslocar: '
 fail_number = 'Somente é permitido sinal de negativo e algarismos.'
 what_accidental = 'Informe o acidente a utilizar: '
 accidental_fail = 'Como acidente somente é permitido # ou b'
-ctrl_v = ''''Transposição realizada.\n
-A versão transposta foi armazenada na área de transferência.\n
+ctrl_v = ''''Transposição realizada.
+A versão transposta foi armazenada na área de transferência.
 (Utilize opção colar ou Ctrl + v)'''
