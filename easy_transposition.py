2c3352da775b9fb9d4d68bf8663aa8dc1b0868ad
pedropolidoro <87273469+pedropolidoro@users.noreply.github.com>
2022-04-24 12:23:45 -0300

    Update easy_transposition.py

--- easy_transposition.py
+++ easy_transposition.py
@@ -2,29 +2,26 @@ import pyperclip
 from time import sleep
 from chord_transpose_tools import chord_sub
 from os import system
+import msg_english as msg
 
 
 system('')
 clear_screen = lambda: print(end="\033[2J")
 
 
-print('Copy the original text (ctrl + c). Waiting.') # 'Copie o texto original (Ctrl + c). Aguardando.' 
+print(msg.ctrl_c)
 original_string = pyperclip.waitForNewPaste()
 clear_screen()
-msg = 'Enter the number of semitones to shift: ' # 'Informe o número de semitons a deslocar: '
-while not (semitones := input(msg)).removeprefix('-').isdecimal():
+while not (semitones := input(msg.num_semitones)).removeprefix('-').isdecimal():
     clear_screen()
-    print('Only negative sign and numerals are allowed.') # 'Somente é permitido sinal de negativo e algarismos.'
+    print(msg.fail_number)
 
 clear_screen()
-msg = 'Report the accidental to use: ' # 'Informe o acidente a utilizar: '
-while not (accidental := input(msg)) in ('#', 'b'):
+while not (accidental := input(msg.what_accidental)) in ('#', 'b'):
     clear_screen()
-    print('Only allowed as accidental # or b') # 'Como acidente somente é permitido # ou b'
+    print(accidental_fail)
 
 pyperclip.copy(chord_sub(original_string, int(semitones), accidental))
 clear_screen()
-print('Transposition was performed.\n' # 'Transposição realizada.\n'
-    'Transposed version was stored in the clipboard.\n' # 'A versão transposta foi armazenada na área de transferência.\n'
-    '(Use paste option or Ctrl + v)') # '(Utilize opção colar ou Ctrl + v)'
+print(msg.ctrl_v)
 sleep(10)
