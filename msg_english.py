09479a295ed1c3a33f4c4b4000939d7805778f77
pedropolidoro <87273469+pedropolidoro@users.noreply.github.com>
2022-04-24 12:41:55 -0300

    Delete msg_english.py

--- msg_english.py
+++
@@ -1,8 +0,0 @@
-ctrl_c = 'Copy the original text (ctrl + c). Waiting.'
-num_semitones = 'Enter the number of semitones to shift: '
-fail_number = 'Only negative sign and numerals are allowed.'
-what_accidental = 'Report the accidental to use: '
-accidental_fail = 'Only allowed as accidental # or b'
-ctrl_v = ''''Transposition was performed.
-Transposed version was stored in the clipboard.
-(Use paste option or Ctrl + v)'''
